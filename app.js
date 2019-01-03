b1cc2616e02af2b66a52833e6509494a3b26e307
vtears <772326244@qq.com>
2019-01-03 13:44:15 +0800

    修改鉴权参数

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const HashStrategy = require('passport-hash').Strategy;
 module.exports = app => {
     const config = app.config.passportHash;
     config.passReqToCallback = true;
+    config.hashParam = 'accesstoken';
+    config.headerField = 'x-auth-token';
 
     app.passport.use(new HashStrategy(config, (req, hash, done) => {
         // 这里不处理应用层逻辑，传给app.passport.verify统一处理
